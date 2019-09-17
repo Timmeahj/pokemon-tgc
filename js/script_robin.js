2e370d206656bd1e75d59a6330a3a654a33b1a6d
RobinMarien <dev.robinmarien@gmail.com>
2019-09-17 10:43:51 +0200

    able to create playerDeck

--- js/script_robin.js
+++ js/script_robin.js
@@ -1,1 +1,40 @@
 // Robin js
+var cardDeck = [];
+var playerDeck = [];
+var lastCard = 69;
+var howManyTimesHaveICalledFetch = 0;
+
+function allCards(callback){
+    for (let i = 1 ; i <= 69; i++){
+        howManyTimesHaveICalledFetch++;
+        fetch("https://api.pokemontcg.io/v1/cards/base1-" + i )
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(card) {
+                howManyTimesHaveICalledFetch--;
+                cardDeck.push(card);
+                if (howManyTimesHaveICalledFetch == 0) {
+                    callback();
+                }
+            });
+    }
+}
+
+allCards(function () {
+    createPlayerDeck();
+    console.log(playerDeck);
+});
+
+function createPlayerDeck(){
+
+    for (let i = 0 ; i <= 5; i++){
+        let random = Math.floor(Math.random() * lastCard);
+        playerDeck.push(cardDeck[random])
+    }
+}
+
+
+
+
+
