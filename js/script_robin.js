835268df500ee9558bb27f3f1204844d779c29a6
RobinMarien <dev.robinmarien@gmail.com>
2019-09-17 15:48:11 +0200

    working image from other api

--- js/script_robin.js
+++ js/script_robin.js
@@ -1,20 +1,22 @@
-// Robin js
-var cardDeck = [];
-var playerDeck = [];
+var cards = [];
 var lastCard = 69;
-var howManyTimesHaveICalledFetch = 0;
+var fetchCount = 0;
 
 function allCards(callback){
-    for (let i = 1 ; i <= 69; i++){
-        howManyTimesHaveICalledFetch++;
+    for (let i = 1 ; i <= lastCard; i){
+// Don't want these
+        if(i == 5 || i == 39 || i == 50){
+            i;
+        }
+        fetchCount++;
         fetch("https://api.pokemontcg.io/v1/cards/base1-" + i )
             .then(function(response) {
                 return response.json();
             })
             .then(function(card) {
-                howManyTimesHaveICalledFetch--;
-                cardDeck.push(card);
-                if (howManyTimesHaveICalledFetch == 0) {
+                fetchCount--;
+                cards.push(card);
+                if (fetchCount == 0) {
                     callback();
                 }
             });
@@ -23,18 +25,17 @@ function allCards(callback){
 
 allCards(function () {
     createPlayerDeck();
-    console.log(playerDeck);
 });
 
 function createPlayerDeck(){
 
-    for (let i = 0 ; i <= 5; i++){
-        let random = Math.floor(Math.random() * lastCard);
-        playerDeck.push(cardDeck[random])
-    }
+    var shuffled = cards.sort(() => 0.5 - Math.random());
+
+    var deck1 = shuffled.slice(0, 20);
+    var deck2 = shuffled.slice(20, 40);
+
+    var hand1 = deck1.slice(0,5);
+    var hand2 = deck2.slice(0,5);
+
+    console.log(deck1, deck2, cards, hand1, hand2);
 }
-
-
-
-
-
